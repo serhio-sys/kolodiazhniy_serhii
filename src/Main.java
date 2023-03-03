08552ee6d173f997e909db24bafedc8701e43d66
ZXCMONSTER <wertunwertun@rambler.ru>
2023-03-03 11:33:16 +0200

    Task01: add doc comments

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Print command-line parameters.
+ */
+public class Main {
+    /**
+     * Program entry point.
+     * @param args command-line parameters list
+     */
+    public static void main(String[] args){
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
