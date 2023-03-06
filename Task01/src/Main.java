12fee9d8e0ebfa9a46ef9fc163d9486ce350abfc
ZXCMONSTER <wertunwertun@rambler.ru>
2023-03-06 14:37:19 +0200

    Task01: add doc comments/modified

--- Task01/src/Main.java
+++ Task01/src/Main.java
@@ -1,6 +1,12 @@
+
+/**
+ * The class Main
+ * This class have a menu of working program
+ */
 public class Main {
 
     public static void main(String[] args){
+    System.out.println("Hello!");
 
     }
 }
